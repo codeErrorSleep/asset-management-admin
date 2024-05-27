badffcf4d5635ab503dae75f38979461f4c27827
elvin.qiu <elvin.qiu@anker-in.com>
2024-05-27 23:23:11 +0800

    feat:初始化链接mysql

--- be/api/internal/handler/basestation/getbasestationhandler.go
+++ be/api/internal/handler/basestation/getbasestationhandler.go
@@ -6,6 +6,7 @@ import (
 	"be/api/internal/logic/basestation"
 	"be/api/internal/svc"
 	"be/api/internal/types"
+
 	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
--- be/api/internal/svc/servicecontext.go
+++ be/api/internal/svc/servicecontext.go
@@ -2,14 +2,20 @@ package svc
 
 import (
 	"be/api/internal/config"
+	"be/model/mysql"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config       config.Config
+	TBaseStation mysql.TBaseStationModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	sqlConn := sqlx.NewMysql(c.Mysql.Datasource)
 	return &ServiceContext{
-		Config: c,
+		Config:       c,
+		TBaseStation: mysql.NewTBaseStationModel(sqlConn),
 	}
 }
